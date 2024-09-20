bd9a056ba827e8e64aa44ce10fc3cf9372f6a23d
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-09-20 14:37:24 -0700

    Tag created resources (#12)

--- locals.tf
+++ locals.tf
@@ -1,5 +1,8 @@
 locals {
   tags = {
+    environment : var.environment
+    service : "terraformer"
+    account : data.aws_caller_identity.current.account_id
     created_by_module : "infrahouse/terraformer/aws"
   }
   ubuntu_versions = {
