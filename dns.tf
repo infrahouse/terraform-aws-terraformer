57cff494786b9ab5535de2fd784b0345ab058fae
xOtanix <maksym@tinyfish.io>
2025-08-29 12:10:02 +0300

    add caa record

--- dns.tf
+++ dns.tf
@@ -7,3 +7,16 @@ resource "aws_route53_record" "terraformer" {
     aws_instance.terraformer.private_ip
   ]
 }
+
+resource "aws_route53_record" "caa_record" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${var.dns_name}.${data.aws_route53_zone.zone.name}"
+  type    = "CAA"
+  ttl     = 300
+  records = [
+    "0 issue \"amazon.com\"",
+    "0 issue \"amazontrust.com\"",
+    "0 issue \"awstrust.com\"",
+    "0 issue \"amazonaws.com\""
+  ]
+}
