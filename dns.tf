b42090c477a8e9e222c125bdae09e6eec8984b32
Maksym Kryva <maksym@tinyfish.io>
2025-09-17 11:15:19 +0300

    add caa record (#25)
    * add caa record
    
    * Update dns.tf
    
    Signed-off-by: Maksym Kryva <maksym@tinyfish.io>
    
    ---------
    
    Signed-off-by: Maksym Kryva <maksym@tinyfish.io>

--- dns.tf
+++ dns.tf
@@ -7,3 +7,14 @@ resource "aws_route53_record" "terraformer" {
     aws_instance.terraformer.private_ip
   ]
 }
+
+resource "aws_route53_record" "caa_record" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${var.dns_name}.${data.aws_route53_zone.zone.name}"
+  type    = "CAA"
+  ttl     = 300
+  records = [
+    "0 issue \"amazon.com\"",
+    "0 issuewild \";\""
+  ]
+}
