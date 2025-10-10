fa3ec02e59c3fcab973ece774e0cc5974a8b2543
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-10-09 21:41:42 -0700

    fix role_arn - can be null

--- test_data/terraformer/providers.tf
+++ test_data/terraformer/providers.tf
@@ -1,9 +1,11 @@
 provider "aws" {
   region = var.region
-  assume_role {
-    role_arn = var.role_arn
-  }
-  default_tags {
+  dynamic "assume_role" {
+    for_each = var.role_arn != null ? [1] : []
+    content {
+      role_arn = var.role_arn
+    }
+  }  default_tags {
     tags = {
       "created_by" : "infrahouse/terraform-aws-terraformer" # GitHub repository that created a resource
     }
