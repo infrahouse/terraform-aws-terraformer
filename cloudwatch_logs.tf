db32be2a36a032db534b376926551a429c1c2430
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2026-01-15 09:40:05 -0800

    fix: improve CloudWatch log group naming and CPU alarm sensitivity
    - Change log group name from static /aws/ec2/terraformer to
      /aws/ec2/terraformer/${environment}/${dns_name} to support
      multiple instances per account (matches jumphost pattern)
    - Increase CPU alarm evaluation_periods from 1 to 2 to reduce
      false positives from temporary spikes
    - Add missing trailing newlines to auto_recovery.tf and cloudwatch_logs.tf
    - Update security.md to reflect new log group naming pattern

--- cloudwatch_logs.tf
+++ cloudwatch_logs.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "terraformer" {
-  name              = "/aws/ec2/terraformer"
+  name              = "/aws/ec2/terraformer/${var.environment}/${var.dns_name}"
   retention_in_days = var.cloudwatch_log_retention
 
   tags = merge(
